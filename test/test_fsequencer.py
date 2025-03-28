dc12b09e61b0f7e0fccfbad78c22900b9b340f66
Jan Herout <jan.herout@o2.cz>
2025-03-28 13:29:15 +0100

    fix: failing test

--- test/test_fsequencer.py
+++ test/test_fsequencer.py
@@ -9,55 +9,41 @@ fixtures_dir = Path(__file__).parent / "fixtures" / "fsequencer" / "db"
 def test_sequencer1():
     batch = fsequencer.create_batch(root_dir=fixtures_dir)
     expected_batch = fsequencer.DeploymentBatch(
-        root_dir=Path("/home/jan/d-blocks/d-blocks-core/test/fixtures/fsequencer/db"),
+        root_dir=fixtures_dir,
         steps=[
             fsequencer.DeploymentStep(
                 name="10-step1",
-                location=Path(
-                    "/home/jan/d-blocks/d-blocks-core/test/fixtures/fsequencer/db/10-step1"
-                ),
+                location=fixtures_dir / "10-step1",
                 files=[
                     fsequencer.DeploymentFile(
                         default_db=None,
-                        file=Path(
-                            "/home/jan/d-blocks/d-blocks-core/test/fixtures/fsequencer/db/10-step1/1.tab"
-                        ),
+                        file=fixtures_dir / "10-step1/1.tab",
                         file_type=meta_model.TABLE,
                     ),
                     fsequencer.DeploymentFile(
                         default_db=None,
-                        file=Path(
-                            "/home/jan/d-blocks/d-blocks-core/test/fixtures/fsequencer/db/10-step1/2.sql"
-                        ),
+                        file=fixtures_dir / "10-step1/2.sql",
                         file_type=meta_model.GENERIC_SQL,
                     ),
                 ],
             ),
             fsequencer.DeploymentStep(
                 name="20-step2",
-                location=Path(
-                    "/home/jan/d-blocks/d-blocks-core/test/fixtures/fsequencer/db/20-step2"
-                ),
+                location=fixtures_dir / "20-step2",
                 files=[
                     fsequencer.DeploymentFile(
                         default_db=None,
-                        file=Path(
-                            "/home/jan/d-blocks/d-blocks-core/test/fixtures/fsequencer/db/20-step2/x-at-the-end.viw"
-                        ),
+                        file=fixtures_dir / "20-step2/x-at-the-end.viw",
                         file_type=meta_model.VIEW,
                     ),
                     fsequencer.DeploymentFile(
                         default_db="db1",
-                        file=Path(
-                            "/home/jan/d-blocks/d-blocks-core/test/fixtures/fsequencer/db/20-step2/db1/1.tab"
-                        ),
+                        file=fixtures_dir / "20-step2/db1/1.tab",
                         file_type=meta_model.TABLE,
                     ),
                     fsequencer.DeploymentFile(
                         default_db="db2",
-                        file=Path(
-                            "/home/jan/d-blocks/d-blocks-core/test/fixtures/fsequencer/db/20-step2/db2/1.tab"
-                        ),
+                        file=fixtures_dir / "20-step2/db2/1.tab",
                         file_type=meta_model.TABLE,
                     ),
                 ],
